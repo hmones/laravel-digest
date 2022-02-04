6910da7a224a373151bf4f3660c7daf7c369a4eb
Haytham Mones <haytham.mones@easybell.de>
2022-02-04 14:44:41 +0100

    Fixed the code smell in the test

--- tests/Feature/AmountDigestTest.php
+++ tests/Feature/AmountDigestTest.php
@@ -2,13 +2,9 @@
 
 namespace Hmones\LaravelDigest\Tests\Feature;
 
-use Hmones\LaravelDigest\Facades\Digest;
 use Hmones\LaravelDigest\Mail\DefaultMailable;
 use Hmones\LaravelDigest\Models\Digest as DigestModel;
 use Hmones\LaravelDigest\Tests\TestCase;
-use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class AmountDigestTest extends TestCase
@@ -19,9 +15,11 @@ class AmountDigestTest extends TestCase
         ['Third Name']
     ];
 
+    protected $thresholdConfKey = 'laravel-digest.amount.threshold';
+
     public function test_digest_emails_are_sent_successfully_after_threshold(): void
     {
-        config(['laravel-digest.amount.threshold' => 3]);
+        config([$this->thresholdConfKey => 3]);
         $this->addEmails($this->testData);
         Mail::assertQueued(DefaultMailable::class, fn ($mail) => $mail->data === $this->testData);
         $this->assertEquals(DigestModel::count(), 0);
@@ -29,7 +27,7 @@ class AmountDigestTest extends TestCase
 
     public function test_digest_emails_are_not_sent_if_threshold_option_not_enabled(): void
     {
-        config(['laravel-digest.amount.threshold' => 3, 'laravel-digest.amount.enabled' => false]);
+        config([$this->thresholdConfKey => 3, 'laravel-digest.amount.enabled' => false]);
         $this->addEmails($this->testData);
         Mail::assertNothingQueued();
         $this->assertEquals(DigestModel::count(), 3);
@@ -37,7 +35,7 @@ class AmountDigestTest extends TestCase
 
     public function test_digest_emails_are_not_queued_if_method_option_is_set_to_send(): void
     {
-        config(['laravel-digest.amount.threshold' => 3, 'laravel-digest.method' => 'send']);
+        config([$this->thresholdConfKey => 3, 'laravel-digest.method' => 'send']);
         $this->addEmails($this->testData);
         Mail::assertSent(DefaultMailable::class, fn ($mail) => $mail->data === $this->testData);
         $this->assertEquals(DigestModel::count(), 0);
