ea535ae0cbae6af90269a3035b487db3401928e3
Haytham Mones <haytham.mones@easybell.de>
2022-02-28 12:41:30 +0100

    Enabled the publishing of package migrations

--- src/LaravelDigestServiceProvider.php
+++ src/LaravelDigestServiceProvider.php
@@ -23,14 +23,11 @@ class LaravelDigestServiceProvider extends ServiceProvider
             __DIR__.'/../config/laravel-digest.php' => config_path('laravel-digest.php'),
         ], 'laravel-digest-config');
 
-        $this->app->afterResolving(Schedule::class, function (Schedule $schedule) {
-            $schedule->command('digest:send daily')->dailyAt(config('laravel-digest.frequency.daily.time'));
-            $schedule->command('digest:send weekly')->weeklyOn(config('laravel-digest.frequency.weekly.day'), config('laravel-digest.frequency.weekly.time'));
-            $schedule->command('digest:send monthly')->monthlyOn(config('laravel-digest.frequency.monthly.day'), config('laravel-digest.frequency.monthly.time'));
-            foreach (Digest::getCustomFrequencies() as $name => $cron) {
-                $schedule->command('digest:send '.$name)->cron($cron);
-            }
-        });
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations'),
+        ], 'laravel-digest-migrations');
+
+        $this->scheduleDigestCommands();
     }
 
     public function register(): void
@@ -43,5 +40,17 @@ class LaravelDigestServiceProvider extends ServiceProvider
     public function provides(): array
     {
         return ['laravel-digest'];
+    }
+
+    protected function scheduleDigestCommands(): void
+    {
+        $this->app->afterResolving(Schedule::class, function (Schedule $schedule) {
+            $schedule->command('digest:send daily')->dailyAt(config('laravel-digest.frequency.daily.time'));
+            $schedule->command('digest:send weekly')->weeklyOn(config('laravel-digest.frequency.weekly.day'), config('laravel-digest.frequency.weekly.time'));
+            $schedule->command('digest:send monthly')->monthlyOn(config('laravel-digest.frequency.monthly.day'), config('laravel-digest.frequency.monthly.time'));
+            foreach (Digest::getCustomFrequencies() as $name => $cron) {
+                $schedule->command('digest:send '.$name)->cron($cron);
+            }
+        });
     }
 }
